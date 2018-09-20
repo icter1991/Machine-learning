ff9794b0718d89110453b810fe62823ffea17fa3
Sebastian Krzeminski <icter1991@gmail.com>
2018-09-20 18:45:44 +0100

    0.0.1.5

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -1,5 +1,5 @@
 import numpy
-from sklearn import preprocessing, cross_validation, neighbors
+from sklearn import preprocessing, model_selection, neighbors
 import pandas
 
 data_frame = pandas.read_csv('breast-cancer-wisconsin.data.txt')
@@ -9,7 +9,7 @@ data_frame.drop(['id'], 1, inplace=True)
 X = numpy.array(data_frame.drop(['class'], 1))
 y = numpy.array(data_frame['class'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 clf = neighbors.KNeighborsClassifier()
 clf.fit(X_train, y_train)
